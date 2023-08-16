aa6b363021dd735794657c767b30077c083b36cb
mo7amed-magdy <136624437+mo7amed-magdy@users.noreply.github.com>
2023-08-16 17:27:53 +0300

    vue-project

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
+    <AddTask v-show="showAddTask" @add-task="addTask" />
+  <Tasks
+    @toggle-reminder="toggleReminder"
+    @delete-task="deleteTask"
+    :tasks="tasks"
+  />
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+// import Tasks from "./components/Tasks.vue"
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+// import AddTask from "./components/AddTask.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      tasks:[],
+      showAddTask: false,
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+     }
+    },
+  }
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.container {
+  min-width: 500px;
+  margin: auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,46 @@
+<template>
+    <div
+      @dblclick="$emit('toggle-reminder', task.id)"
+      :class="[task.reminder ? 'reminder' : '']"
+      class="task"
+    >
+      <h3>
+        {{ task.text }}
+        <i @click="$emit('delete-task', task.id)" class="fa-solid fa-circle-xmark"></i>
+      </h3>
+      <p>{{ task.day }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Task',
+    props: {
+      task: Object,
+    },
+  }
+  </script>
+  
+  <style scope>
+  .fa-solid {
+    color: red;
+  }
+  
+  .task {
+    /* background: #f4f4f4; */
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  
+  .task.reminder {
+    border-left: 5px solid green;
+  }
+  
+  .task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  </style>
+  
